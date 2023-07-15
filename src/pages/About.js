ff1004a1404af970e01b6d22d298daa372a53530
MikheilPapundze <mishikopapunidze@gmail.com>
2023-07-15 00:21:25 -1200

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis nulla,
+        laudantium unde fuga quod molestiae quo mollitia vitae corporis
+        voluptates natus soluta? Nulla perspiciatis ea excepturi amet optio
+        magni pariatur, laudantium voluptate, reprehenderit quam non aut vero
+        et. Quam, ex.
+      </p>
+    </section>
+  );
+};
+
+export default About;
